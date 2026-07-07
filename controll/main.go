c0e74f8a40c1209ed7c65246e9500be498e5bb78
Miguel <migueelcoi@gmail.com>
2026-07-07 16:54:29 -0400

    arquivos grpc adicionado

--- controll/main.go
+++ controll/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/coigo/micro-cloud/commandservice"
+	proto "github.com/coigo/micro-cloud/controll/proto/status_receiver"
+	"google.golang.org/grpc"
 )
 
+type server struct {
+	proto.s *server
+}
+
 func main () {
-	dockerId := commandservice.UpCommand()
-	fmt.Printf("Container %v criado.\n", dockerId)
-	commandservice.DownCommand(dockerId)
+	// dockerId := commandservice.UpCommand()
+	// fmt.Printf("Container %v criado.\n", dockerId)
+	// commandservice.DownCommand(dockerId)
 }
